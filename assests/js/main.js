a19111e9d0391ab024ee623191f0af99181187a1
sonisaini901 <soni@iosandweb.net>
2025-05-20 18:30:58 +0530

    code

--- assests/js/main.js
+++ assests/js/main.js
@@ -55,4 +55,94 @@ jQuery(document).ready(function($){
             }
         ]
     });
+
+    // Related Product Slider
+    $('.related_products_slider').slick({
+        infinite: true,
+        dots: false,
+        slidesToScroll: 1,
+        slidesToShow: 4,
+        adaptiveHeight: false,
+        autoplay: false,
+        arrows: true,
+        prevArrow: $(".related_back_arrow"),
+        nextArrow: $(".related_next_arrow"),
+        responsive: [
+            {
+                breakpoint: 1200,
+                settings: {
+                    slidesToShow: 3
+                }
+            },
+            {
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 2
+                }
+            },
+            {
+                breakpoint: 578,
+                settings: {
+                    slidesToShow: 1
+                }
+            }
+        ]
+    });
+
+    // Poduct Variant select
+    $('input[type=radio][name=variant]').on('change', function() {
+        if ($(this).is(':checked')) {
+            $('.btn-radio-select').removeClass('active');
+            $(this).parent('.btn-radio-select').addClass('active');
+        } else {
+            $('.btn-radio-select').removeClass('active');
+        }
+        
+    });
+
+    // Product gallery slider
+    if($('.product_slider_selection')){
+        var thumbs = $('.product_slider_selection').find('img');
+
+        thumbs.click(function(){
+            var src = $(this).attr('src');
+            var dp = $('.product-display-img');
+            var img = $('.zoom');
+            dp.attr('src', src);
+            img.attr('src', src);
+        });
+
+        $(".product-thumbnail").click(function(){
+            $('.product-thumbnail').removeClass('selected');
+            $(this).addClass('selected');
+        });
+
+        var zoom = $('.product-large-image').find('img').attr('src');
+        $('.product-large-image').append('<img class="zoom" src="'+zoom+'">');
+        $('.product-large-image').mouseenter(function(){
+            $(this).mousemove(function(event){
+                var offset = $(this).offset();
+                var left = event.pageX - offset.left;
+                var top = event.pageY - offset.top;
+                
+                $(this).find('.zoom').css({
+                    width: '200%',
+                    opacity: 1,
+                    left: -left,
+                    top: -top,
+                });
+            });
+        });
+
+        $('.product-large-image').mouseleave(function(){
+            $(this).find('.zoom').css({
+                width: '100%',
+                opacity: 0,
+                left: 0,
+                top: 0,
+            });
+        });
+    }
+
+    
 });
